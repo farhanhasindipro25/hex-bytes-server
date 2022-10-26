98f14b24a7b61aa28e0ea0457e5b398cc8dcee3f
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-10-26 21:20:07 +0600

    Course Categories API created on local host: 5000

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+app.use(cors());
+
+const courseCategories = require('./data/courseCategories.json');
+const courseDetails = require('./data/courseDetails.json');
 
 app.get("/", (req, res) => {
   res.send("HexBytes API Running");
 });
+
+// Making the API for Course Categories
+app.get('/course-categories', (req,res)=>{
+    res.send(courseCategories);
+})
+
+
 
 app.listen(port, () => {
   console.log("HexBytes Server running on port:", port);
